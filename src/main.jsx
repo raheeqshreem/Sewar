0b9dcca2cab2d7b5e149e36b4c59037fe93a2d36
raheeq shreem <shreemraheeq@gmail.com>
2025-09-20 01:37:30 +0300

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
  import { Toaster } from "react-hot-toast";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <Toaster position="top-right"/>
-  <BrowserRouter>
-<GoogleOAuthProvider clientId="752427255311-9o7ldud5rmr2neecl1o055ugqd3e5ogi.apps.googleusercontent.com">
+  <HashRouter>
+<GoogleOAuthProvider clientId="752427255311-2e9hmpg8qu03vutegp3o8bi50097ahkt.apps.googleusercontent.com">
         <App />
       </GoogleOAuthProvider>
-       </BrowserRouter>
+       </HashRouter>
  </StrictMode>
 );
